7d68c30f23b9bf75500372589bbfae3d4336aef9
George Thomas <george@georges-mbp.home>
2019-01-27 17:28:10 +0000

    test(operator): Tests to define output from operator executing mission on a planet

--- src/operator.js
+++ src/operator.js
@@ -1,15 +1,26 @@
+const { createRobotState, rotateRobot, moveRobot } = require('./robot');
 
-function executeRobotMission({planetState, missionInstructions}) {
-	// Create the initial state of the robot
-	// Iterate over the commands
-	// Deploy scent
-    // Move the robot
-    // Check if position is off world
-       // If off world the declare such and cease commands
-       // If on world remove the scent
-    // Return the latest planet and robot state
+
+function executeRobotMission({ planetState, mission }) {
+  const initialRobotState = createRobotState(mission.initial);
+
+  // mission.commands.re
+
+  // Create the initial state of the robot
+  // Iterate over the commands
+  // Deploy scent
+  // Move the robot
+  // Check if position is off world
+  // If off world the declare such and cease commands
+  // If on world remove the scent
+  // Return the latest planet and robot state
+
+  return {
+    planetState,
+    robotState: initialRobotState
+  }
 }
 
 module.exports = {
-	executeRobotMission
-}
+  executeRobotMission,
+};
---
+++ test/operator/executeRobotMission.test.js
@@ -0,0 +1,116 @@
+const test = require('ava');
+const { executeRobotMission } = require('../../src/operator');
+
+function verifyRobotState({ t }, { finalState, expectedState }) {
+  t.deepEqual(finalState.robotState.position, expectedState.robot.position);
+  t.is(finalState.robotState.orientation, expectedState.robot.orientation);
+  t.is(finalState.robotState.lost, expectedState.robot.lost);
+}
+
+test('Should navigate the robot along a route that is all on world', (t) => {
+  const initial = {
+    position: [1, 1],
+    orientation: 'E',
+  };
+
+  const instructions = ['R', 'F', 'R', 'F', 'R', 'F', 'R', 'F'];
+
+  const mission = {
+    initial,
+    instructions,
+  };
+
+  const planetState = {
+    bounds: [5, 3],
+    scents: [],
+  };
+
+  const expectedRobotState = {
+    position: [1, 1],
+    orientation: 'E',
+    lost: false,
+  };
+
+  const expectedState = {
+    robot: {
+      position: [1, 1],
+      orientation: 'E',
+      lost: false,
+    },
+    planet: planetState
+  }
+
+  const finalState = executeRobotMission({ planetState, mission });
+
+  verifyRobotState({ t }, { finalState, expectedState });
+});
+
+test('Should navigate the robot along a route that takes it off world', (t) => {
+  const initial = {
+    position: [3, 2],
+    orientation: 'N',
+  };
+
+  const instructions = ['F', 'R', 'R', 'F', 'L', 'L', 'F', 'F', 'R', 'R', 'F', 'L', 'L'];
+
+  const mission = {
+    initial,
+    instructions,
+  };
+
+  const planetState = {
+    bounds: [5, 3],
+    scents: [],
+  };
+
+  const expectedState = {
+    robot: {
+      position: [3, 3],
+      orientation: 'N',
+      lost: true,
+    },
+    planet: {
+      bounds: planetState.bounds,
+      scents: ['3-3-N']
+    }
+  }
+
+  const finalState = executeRobotMission({ planetState, mission });
+
+  verifyRobotState({ t }, { finalState, expectedState });
+});
+
+test('Should complete this other mission', (t) => {
+  const initial = {
+    position: [0, 3],
+    orientation: 'W',
+  };
+
+  const instructions = ['L', 'L', 'F', 'F', 'F', 'L', 'F', 'L', 'F', 'L'];
+
+  const mission = {
+    initial,
+    instructions,
+  };
+
+  const planetState = {
+    bounds: [5, 3],
+    scents: ['3-3-N'],
+  };
+
+  const expectedState = {
+  	robot: {
+	    position: [2, 3],
+	    orientation: 'S',
+	    lost: false,
+    },
+    planet: {
+      bounds: planetState.bounds,
+      scents: ['3-3-N']
+    }
+	}
+
+  const finalState = executeRobotMission({ planetState, mission });
+
+  verifyRobotState({ t }, { finalState, expectedState });
+});
